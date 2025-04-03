fd34987e2d33f39bd984af309ecb438252b9ebe3
rana reda roshdy <ranaredaroshdy.com>
2025-04-03 21:06:28 +0200

    lol

---
+++ Area_volume.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+float r;
+float surface_area,volume;
+const float PI = 3.14;6
+cout <<"Enter radius of the sphere : ";
+cin >>r;
+surface_area = 4* PI * r * r;
+volume = (4.0/3) * PI * r * r * r;
+cout<<"Surface area of sphere is:"<<surface_area;
+cout<<"\nVolume of sphere is : "<<volume;
+return 0;
+}
